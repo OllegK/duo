f31a004df08aaf3eded663e7d9e40a0aa752e96a
OllegK <oleg.koleskin@gmail.com>
2019-03-09 22:27:50 +0200

    adding icons

--- webpack.config.js
+++ webpack.config.js
@@ -36,13 +36,48 @@ const options = {
       staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
     }),
     new WebpackPwaManifest({
-      "short_name": "React App",
-      "name": "Create React App Sample",
+      "short_name": "Duo",
+      "name": "Duo Game",
       "icons": [
         {
-          "src": "./public/favicon.ico",
-          "sizes": "64x64 32x32 24x24 16x16",
-          "type": "image/x-icon"
+          "src": "public/icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "public/icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
         }
       ],
       "start_url": "./",
