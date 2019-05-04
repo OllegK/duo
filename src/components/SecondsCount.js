1a4d22aba568900a27d5896e32ad3164265042c6
OllegK <oleg.koleskin@gmail.com>
2019-05-04 20:45:02 +0300

    Adding conditional message

--- src/components/SecondsCount.js
+++ src/components/SecondsCount.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import './SecondsCount.css';
 
 const SecondsCount = props => (
-  <div className="seconds">
-    <button type="button" disabled={props.seconds <= 1} onClick={() => props.decSeconds()}>-</button>
-    <span className="secondsSpan">
-      {props.seconds}
-      &nbsp;
-      Seconds
+  <>
+    <div className="seconds">
+      <button type="button" disabled={props.seconds <= 1} onClick={() => props.decSeconds()}>-</button>
+      <span className="secondsSpan">
+        {props.seconds}
+        &nbsp;
+        Seconds
     </span>
-    <button type="button" onClick={() => props.incSeconds()}>+</button>
-  </div>
+      <button type="button" onClick={() => props.incSeconds()}>+</button>
+    </div>
+    <div className="seconds">
+      {props.seconds <= 5 && <span className="secondsSpan">You are kidding me!</span>}
+    </div>
+  </>
 );
 
 SecondsCount.propTypes = {
